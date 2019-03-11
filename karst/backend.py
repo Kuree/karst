13d3b7dec926120dfacbc6a2c5145f21e20ebca9
Keyi Zhang <keyi@stanford.edu>
2019-03-10 23:40:05 -0700

    add linear spacing in z3

--- karst/backend.py
+++ karst/backend.py
@@ -3,8 +3,15 @@ from typing import Dict
 import z3
 
 
-def construct_sym_expr_tree(expression: Expression,
+def construct_sym_expr_tree(expression: Union[Expression, Variable],
                             symbol_table: Dict[str, z3.Int]):
+    if isinstance(expression, Const):
+        return expression.value
+    elif isinstance(expression, Variable):
+        if expression.name not in symbol_table:
+            symbol_table[expression.name] = z3.Int(expression.name)
+        return symbol_table[expression.name]
+
     left = expression.left
     right = expression.right
 
@@ -61,3 +68,45 @@ def is_exclusive_condition(*args):
 
     r = s.check()
     return r == z3.unsat
+
+
+def __abs(x):
+    return z3.If(x >= 0, x, -x)
+
+
+def get_linear_spacing(*args):
+    # this function only handles one single variable
+    symbol_table = {}
+    expressions = []
+    for exp in args:
+        smt_expr = construct_sym_expr_tree(exp, symbol_table)
+        expressions.append(smt_expr)
+    assert len(symbol_table) == 1, "Only one variable allowed"
+    sym = symbol_table.popitem()
+    ints = [0]
+    assert sym is not None
+    for idx, exp1 in enumerate(expressions):
+        for exp2 in expressions[idx + 1:]:
+            r = z3.simplify(__abs(exp1 - exp2))
+            if not isinstance(r, z3.IntNumRef) or not r.is_int():
+                return False
+            ints.append(r.as_long())
+    int_set = set(ints)
+    opt = z3.Optimize()
+    gcd = z3.Int("gcd")
+    for idx, i in enumerate(int_set):
+        v = z3.Int(f"int_{idx}")
+        opt.add(v * gcd == i)
+    opt.maximize(gcd)
+    r = opt.check()
+    if r != z3.sat:
+        return False
+    result = opt.model()
+    value = result.eval(gcd).as_long()
+    # this is the actual gcd
+    # now it's just a simple math trick to verify these are arithmetic
+    # sequence
+    minimum = min(int_set)
+    int_sum = sum(int_set)
+    expected_sum = minimum + (len(int_set) - 1) * value * len(int_set) // 2
+    return int_sum == expected_sum
--- tests/test_backend.py
+++ tests/test_backend.py
@@ -15,4 +15,15 @@ def test_exclusive_expression():
     exp2 = b + a > 5
 
     assert not is_exclusive_condition(exp1, exp2)
-    assert is_exclusive_condition(a > 6, b < 3, a - b < 0)
+    assert is_exclusive_condition(a > 6, b < 3, a < b)
+
+
+def test_linear_spacing():
+    parent = MemoryModel(10)
+    a = Variable("a", 16, parent)
+
+    assert get_linear_spacing(a, a + Const(1), a + Const(2))
+    assert get_linear_spacing(a, a + Const(2), a + Const(1))
+    assert get_linear_spacing(a + 1, a + 4, a + 7)
+
+    assert not get_linear_spacing(a, a + 3, a + 5)
